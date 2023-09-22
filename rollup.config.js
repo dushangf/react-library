20213bc404d695709127247136b3f56f3d8b66d9
dushangfk <dushan.fernando@kochasoft.lk>
2023-09-22 21:01:07 +0530

    gitignore updated

--- rollup.config.js
+++ rollup.config.js
@@ -8,7 +8,7 @@ const packageJson = require('./package.json');
 
 export default [
   {
-    input: 'src/packages.tsx',
+    input: 'src/index.tsx',
     output: [
       {
         file: packageJson.main,
